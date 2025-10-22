41c147a0621e13b845a6f08365848fdd11aeef96
Priyaskoirala <priyaskoirala10@gmail.com>
2025-10-22 22:44:47 +0100

    t8

--- sw.js
+++ sw.js
@@ -1,32 +1,38 @@
-const CACHE_NAME = `temperature-converter-v1`;
+const CACHE_NAME = 'temperature-converter-v1';
+
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
-event.waitUntil((async () => {
-const cache = await caches.open(CACHE_NAME);
-cache.addAll([
-'/',
-'/converter.js',
-'/converter.css'
-]);
-})());
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll([
+      './',
+      './index.html',
+      './converter.js',
+      './converter.css',
+      './manifest.json',
+      './icon512.png'
+    ]);
+    console.log('Service worker: files cached');
+  })());
 });
+
 self.addEventListener('fetch', event => {
-event.respondWith((async () => {
-const cache = await caches.open(CACHE_NAME);
-// Get the resource from the cache.
-const cachedResponse = await cache.match(event.request);
-if (cachedResponse) {
-return cachedResponse;
-} else {
-try {
-// If the resource was not in the cache, try the network.
-const fetchResponse = await fetch(event.request);
-// Save the resource in the cache and return it.
-cache.put(event.request, fetchResponse.clone());
-return fetchResponse;
-} catch (e) {
-// The network failed.
-}
-}
-})());
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+
+    // Try cache first
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    }
+
+    // Otherwise, try the network and cache new files
+    try {
+      const fetchResponse = await fetch(event.request);
+      cache.put(event.request, fetchResponse.clone());
+      return fetchResponse;
+    } catch (e) {
+      console.log('Fetch failed; returning offline page if available.');
+    }
+  })());
 });
